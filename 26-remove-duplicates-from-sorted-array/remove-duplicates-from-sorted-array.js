bb5dbb5e7452d092392791bc6482fcdb8eb977a9
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-08 00:20:21 +1200

    Time: 66 ms (48.79%) | Memory: 51.6 MB (92.23%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -6,13 +6,14 @@ var removeDuplicates = function(nums) {
     let result = [];
     let duplicate = [];
     nums.forEach(n => {
+        //Remove if statement to improve runtime
         !result.includes(n) && result.push(n)
     })
-    for(let i = 0; i<result.length; i++){
-        nums[i] = result[i]
-    }
-    for(let i = result.length; i < nums.length; i++) {
-        nums[i] = "_"
+    // for(let i = 0; i<result.length; i++){
+    //     nums[i] = result[i]
+    // }
+    for(let i = 0; i < nums.length; i++) {
+        result[i] === 0 || result[i] ? nums[i] = result[i] : nums[i] = "_";
     }
     return result.length;
 };
