a6e16818efb8050ca6435d95f5038836c299bfab
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-08 00:04:16 +1200

    Time: 62 ms (66.81%) | Memory: 52.4 MB (46.95%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let result = [];
+    let duplicate = [];
+    nums.forEach(n => {
+        if(!result.includes(n)) {result.push(n)}
+    })
+    for(let i = 0; i<result.length; i++){
+        nums[i] = result[i]
+    }
+    for(let i = result.length; i < nums.length; i++) {
+        nums[i] = "_"
+    }
+    return result.length;
+};
