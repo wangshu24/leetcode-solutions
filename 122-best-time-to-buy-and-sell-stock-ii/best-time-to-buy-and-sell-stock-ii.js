933a1191caffd7651445a19f16f4b9ff28d9b0a3
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-12 04:12:06 +1200

    Time: 52 ms (72.23%) | Memory: 49.4 MB (46.74%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -6,11 +6,11 @@ var maxProfit = function(prices) {
     let profit =0;
     let trnding = false;
     prices.forEach((p, i) => 
-                  {
-                    if(prices[i+1] && (prices[i+1]-p)>0){
-                        profit+=prices[i+1]-p
-                    }
+        {
+        if(prices[i+1] && (prices[i+1]-p)>0){
+            profit+=prices[i+1]-p
+        }
     }
-                  )
+    )
     return profit;
 };
