726d75060d7138f5b55c2378ca89bf56e74a2ad2
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-16 14:58:58 +1200

    Time: 109 ms (19.19%) | Memory: 65.6 MB (15.11%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -6,8 +6,7 @@ var findDuplicates = function(nums) {
     let freq = {}
     let result = []
     for(let i of nums){
-        freq[i] = (freq[i] || 0) + 1;
-        
+        freq[i] = (freq[i] || 0) + 1;       
     }
     for(let x in freq){
         if(freq[x]>1) {result.push(x)}
