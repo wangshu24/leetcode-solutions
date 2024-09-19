e57fbe31806f3a443a6a1c258ec1cd7a73c5d25b
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-19 13:25:00 +1200

    Time: 79 ms (5.00%) | Memory: 50.9 MB (56.67%) - LeetSync

---
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var smallestNumber = function(num) {
+    let strNum = num.toString()
+    let strArr = []
+    let isNegative = false
+    let result = ""
+    if(strNum[0] === "-"){
+        isNegative = true;
+        for(let i = 1; i < strNum.length; i++){
+            strArr.push(strNum[i])
+        }
+    } else {
+          for(let i = 0; i < strNum.length; i++){
+            strArr.push(strNum[i])
+        }
+    }
+    if(isNegative){
+        strArr.sort((a,b)=> b-a)
+        result+="-"
+        for(let i of strArr){
+            result+=i.toString()
+        }
+    } else {
+        strArr.sort((a,b)=> a-b)
+        console.log(strArr)
+        if(strArr[0] === "0" && strArr.length > 1){
+            let firstPositiveIntIndex = strArr.findIndex(i => parseInt(i) > 0)
+            strArr[0] = strArr[firstPositiveIntIndex]
+            strArr[firstPositiveIntIndex] = "0"
+        }
+
+        for(let i of strArr){
+            result+=i.toString()
+        }
+    }
+   
+    return parseInt(result)
+};
