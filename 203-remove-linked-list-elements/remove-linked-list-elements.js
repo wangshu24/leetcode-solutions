3637837a9536ff9467fec8d2d9c84627402c0662
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-21 22:33:02 +1200

    Time: 63 ms (90.32%) | Memory: 54.4 MB (21.86%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    let temp = new ListNode(0) 
+    let modifiable = temp
+    temp.next = head
+    while(modifiable.next !== null){
+        if(modifiable.next.val === val) {modifiable.next = modifiable.next.next}
+        else {modifiable = modifiable.next}
+    }
+    
+   return temp.next  
+};
+
