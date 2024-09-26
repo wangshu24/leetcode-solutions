05b3e8995012be924895717d9dce4281aadd63c2
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-26 23:09:32 +1200

    Time: 89 ms (26.83%) | Memory: 52.9 MB (38.40%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -4,7 +4,12 @@
  */
 var mySqrt = function(x) {
     let result
+    //First we handle edge case where x = 0 
     if(x === 0) return 0
+
+    //We setup the 2 tailend range that would contain our potential root 
+    //Incrementally guess the root by doing binary search using the averageMean of 2 end averageMean
+    //If square of averageMean is larger than x, we can lower the right end, vice versa till we inch closer to the true root
     left = 0
     right = x
     while(left <= right){
