c84a39b473819f9bc88c37ba9a574edfb3028379
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-28 10:44:58 +1200

    Time: 61 ms (29.25%) | Memory: 51.8 MB (12.16%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -2,14 +2,60 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    let prefix = "";
-    const sorted = strs.sort();
-    const longestStr = sorted[sorted.length-1]
-    for(let i=0; i < longestStr.length; i++){
-        if(strs.every((word) => word[i] === longestStr[i])){
-            prefix+=longestStr[i];
-        } else {break}
+
+class TrieNode {
+    constructor(){
+        this.children = {}
+        this.count = 0
     }
+}
+
+class Trie {
+    constructor(){
+        this.root = new TrieNode()
+    }
+
+    insert(word){
+        let node = this.root
+        for(const char of word){
+            if(!node.children[char]){
+                node.children[char] = new TrieNode()
+            }
+            node = node.children[char]
+            node.count++
+        }
+    }
+
+    getLongestCommonPrefix(word, num){
+        let node = this.root
+        let result = ""
+        // console.log(node)
+        // console.log(node)
+        // while(node[Object.keys(node)[0]].count === num){
+        //    result+=Object.keys(node)[0]
+        //    node = node[Object.keys(node)[0]]
+        //    node = node.children
+        // }
+        for(const char of word){
+            if(node.children[char]){
+                if( node.children[char].count === num){
+                    result+=char
+                    node = node.children[char]
+                }
+            }
+        }
+
+        return result
+    }
+}
+
+var longestCommonPrefix = function(strs) {
+    let trie = new Trie()
+    for(const word of strs){
+        trie.insert(word)
+    }
+
+    const prefix = trie.getLongestCommonPrefix(strs.sort()[strs.length-1], strs.length) 
+
     return prefix
 };
