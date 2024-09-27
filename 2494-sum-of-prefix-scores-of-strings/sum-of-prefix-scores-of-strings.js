b1d9f4e0e01a194d25657f27ae0892f468f6b005
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-27 22:44:25 +1200

    Time: 1273 ms (48.19%) | Memory: 154.9 MB (63.75%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.js
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.js
@@ -3,33 +3,40 @@
  * @return {number[]}
  */
 
-class TrieNode{
-    constructor() {
+
+class TrieNode {
+    constructor(){
         this.children = {}
         this.prefixCount = 0
     }
 }
 
-class Trie{
+class Trie {
     constructor(){
+        //when trie is instantiated, always start first TrieNode for all words
         this.root = new TrieNode()
     }
 
     insert(word){
+        //start from the same trie node, to compare if new word follows the same path or new path (order) is created
         let node = this.root
         for(const char of word){
             if(!node.children[char]){
+                //check for existence of char, in order
                 node.children[char] = new TrieNode()
             }
+            //reset root to the latest char being inspected to maintain order of char, hence suitable to count and track prefix
             node = node.children[char]
             node.prefixCount++
         }
     }
 
     getPrefixCount(word){
+        //after all insert has been done, all word has been filtered and check for order and repetition, hence, if same prefix has occured, will be counted, hence we just have to tally up
         let node = this.root
         let count = 0
         for(const char of word){
+            //this is following the exact order of prefix, hence any prefix that has occurred to this exact char being inspected has been recorded, now we tally up
             node = node.children[char]
             count += node.prefixCount
         }
@@ -38,30 +45,12 @@ class Trie{
 }
 
 var sumPrefixScores = function(words) {
-    //Time limit exceeded
-    // let result = []
-    // let prefixes = {}
-    // for(let x = 0; x < words.length; x++){
-    //     let prefixSum = 0
-    //     for(let y = 1; y <= words[x].length; y++){
-    //         prefix = words[x].slice(0,y)
-    //         if(!prefixes[prefix]){
-    //             prefixSum += words.filter((word) => word.slice(0,y) === prefix).length
-    //             prefixes[prefix] = prefixSum
-    //         } else {
-    //             prefixSum= prefixes[prefix]
-    //         }
-    //     }
-    //     result.push(prefixSum)
-    // }
-    // return result
-
-    let trie = new Trie()
+    trie = new Trie()
     for(const word of words){
         trie.insert(word)
-        console.log("at word: ", word, " trie is : " ,trie)
     }
-    //console.log(trie.root.children.a.children.b)
-    const result = words.map(word => trie.getPrefixCount(word))
-    return result
+
+    const res = words.map(word => trie.getPrefixCount(word))
+
+    return res;
 };
