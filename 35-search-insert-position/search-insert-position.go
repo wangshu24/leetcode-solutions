d20ad1320b96b3a852823517914dc0bc56752488
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-28 23:18:04 +1200

    Time: 4 ms (9.37%) | Memory: 3 MB (99.03%) - LeetSync

--- 35-search-insert-position/search-insert-position.go
+++ 35-search-insert-position/search-insert-position.go
@@ -1,8 +1,6 @@
 func searchInsert(nums []int, target int) int {
     newNums := append(nums, target)
     slices.Sort(newNums)
-    result := slices.IndexFunc(newNums, func(n int) bool {
-		return n == target
-	})
+    result := slices.Index(newNums, target)
     return result
 }
