243a8dcfe6cd35c7f4d918fc4d52032e8417dec8
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-29 14:00:36 +1300

    Time: 2 ms (68.09%) | Memory: 2.5 MB (71.41%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.go
+++ 88-merge-sorted-array/merge-sorted-array.go
@@ -1,9 +1,8 @@
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    for i:=0; i < n; i++ {
-        fmt.Println(len(nums1))
-        nums1 = slices.Replace(nums1, m+i, m+i+1, nums2[i])
-    }
+    nums1 = append(nums1[:m], nums2...)
     slices.Sort(nums1)
+
+    //why does this not work???
     // for i:=0; i < n; i++ {
     //     nums1 = append(nums1, nums2[i])
     // }
