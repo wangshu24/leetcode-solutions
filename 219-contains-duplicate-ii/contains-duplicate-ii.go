c7dd1abf742577284b12d8666e85d57411a2bf6d
Elias Tran <trandinhphuc1997@gmail.com>
2024-09-30 18:23:29 +1300

    Time: 138 ms (16.49%) | Memory: 18.4 MB (12.37%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.go
@@ -0,0 +1,33 @@
+func containsNearbyDuplicate(nums []int, k int) bool {
+    col := map[int][]int{}
+    result := false
+    for i:=0; i < len(nums);i++ {
+        col[(nums[i])] = append(col[(nums[i])], i)
+    }
+    
+    for _, v := range col {
+        if len(v) >1 {
+            // for i,j := range v {
+            //     if i+1<len(v) {
+            //         fmt.Println(v[i+1])
+            //         fmt.Println(j)
+            //         if ((v[i+1] - v[i]) <= k) {
+            //             fmt.Println("got here")
+            //             result = true
+            //             break
+            //         }
+            //     }
+            // }
+            for i := 0; i < len(v)-1; i++ {
+                fmt.Println(v[i+1])
+                fmt.Println(v[i])
+                if (int(v[i+1]) - int(v[i])) <= k {
+                    result = true
+                    break
+                }
+            } 
+        }
+    }
+
+    return result
+}
