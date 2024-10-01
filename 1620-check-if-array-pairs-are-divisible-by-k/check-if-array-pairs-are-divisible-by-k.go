34391096b903764c7bfc13ba15590041cff937cd
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-02 00:15:26 +1300

    Time: 106 ms (23.53%) | Memory: 11.7 MB (52.94%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.go
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.go
@@ -3,7 +3,6 @@ func canArrange(arr []int, k int) bool {
     for i:=0; i< len(arr); i++ {
         remain[((arr[i]%k)+k)%k]++
     }
-    fmt.Println(remain)
 
     for key,val := range remain {
         if key == 0 { if val%2 != 0 {
@@ -13,45 +12,6 @@ func canArrange(arr []int, k int) bool {
             if remain[k-key] != val {return false}
         }
     }
-
     return true
 }
-
-
-
-
-
-
-
-
-
-// func canArrange(arr []int, k int) bool {
-//     slices.Sort(arr)
-//     largeArr := []int{}
-//     smallArr  := []int{}
-//     result := true
-//     for i:=len(arr)-1;i >-1;i-- {
-//         if arr[i]>k {largeArr = append(largeArr, arr[i])} else {smallArr = append(smallArr, arr[i])}
-//     }
-  
-//     fmt.Println(smallArr)
-//     fmt.Println(largeArr)
-    
-
-//     for i:=0; i<len(largeArr); i++ {
-//         fmt.Println("got here")
-//         if largeArr[i]%k == 0 {
-//             if !slices.ContainsFunc(smallArr, func(n int) bool {return n%k == 0}) {return  false}
-//         } else {
-//             if !slices.ContainsFunc(smallArr, func(n int) bool {return n == largeArr[i]%k}) {return false}
-//         }
-//     } 
-//     if len(largeArr) == 0 {
-//         for i:=0;i < len(smallArr); i++ {
-//             if !slices.ContainsFunc(smallArr, func(n int) bool {return n == (k - smallArr[i])}) {return false}
-//         }
-//     }
-
-//     return result
-// }
 
