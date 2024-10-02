5e1765064e0aee854531f9fd4561451e10f24943
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-02 23:28:40 +1300

    Time: 19 ms (8.26%) | Memory: 5.6 MB (18.04%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -2,21 +2,13 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
     for i:=0; i< len(nums2); i++ {
         nums1 = append(nums1, nums2[i])
     }
-    fmt.Println(nums1)
     slices.Sort(nums1)
     var result float64
-    // for i:=0; i < len(nums1); i++ {
-    //     result+=float64(nums1[i])
-    // }
-    // fmt.Println(result)
-    // result = result/float64(len(nums1))
     if len(nums1)%2 == 1 {
         result = float64(nums1[(len(nums1)-1)/2])
     } else {
         medianS := float64(nums1[len(nums1)/2 -1])
         medianL := float64(nums1[len(nums1)/2 ])
-        fmt.Println(medianS)
-        fmt.Println(medianL)
         result = (medianS + medianL) /2
     }
     return result
