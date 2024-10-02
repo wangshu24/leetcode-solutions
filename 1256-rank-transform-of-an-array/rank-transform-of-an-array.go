f4430fea112261e4f57a05f5ec1265e006b6d6b8
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-02 15:52:07 +1300

    Time: 51 ms (86.79%) | Memory: 12.3 MB (39.62%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.go
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.go
@@ -2,7 +2,6 @@ func arrayRankTransform(arr []int) []int {
     orig := slices.Clone(arr)
  
     slices.Sort(arr)
-    fmt.Println(arr)
     rankMap := map[int]int{}
     rank := 1
     for i:=0; i < len(arr); i++ {
@@ -12,7 +11,6 @@ func arrayRankTransform(arr []int) []int {
             }
         
     }
-    fmt.Println(rankMap)
     for i:=0; i < len(arr); i++ {
         orig[i] = rankMap[orig[i]]
     }
