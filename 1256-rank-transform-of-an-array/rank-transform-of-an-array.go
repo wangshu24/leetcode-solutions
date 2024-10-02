39e5f763803295e28274a9e78dd415525e127044
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-02 21:10:29 +1300

    Time: 39 ms (100.00%) | Memory: 11.2 MB (64.15%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.go
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.go
@@ -8,8 +8,7 @@ func arrayRankTransform(arr []int) []int {
         if rankMap[arr[i]] ==  0 {
             rankMap[arr[i]] = rank
             rank++
-            }
-        
+            }    
     }
     for i:=0; i < len(arr); i++ {
         orig[i] = rankMap[orig[i]]
