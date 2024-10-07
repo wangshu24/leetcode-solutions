f442422b95677f0633fd538d72339a82b146d36a
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-07 21:50:44 +1300

    Time: 3 ms (65.91%) | Memory: 3.8 MB (34.09%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.go
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.go
@@ -1,30 +1,36 @@
-// func minLength(s string) int {
-//     for ok, subStr := hasEither(s); ok {
-//         strings.Replace(s, subStr, "", 1)
-//     }
-//     fmt.Println(s)
-//     return 0
-// }
-
-// func hasEither(s string) (bool, string){
-//     if strings.Contains(s, "AB"){
-//         return true, "AB"
-//     } else if strings.Contains(s, "CD"){
-//         return true, "CD"
-//     } else {return false, ""}
-// }
-
-
 func minLength(s string) int {
-    if !strings.Contains(s, "AB") && !strings.Contains(s, "CD") {return len(s)}
-    found := true
-    for found {
-        if strings.Contains(s, "AB"){
-            s = strings.Replace(s, "AB", "", 1)
-        } else if strings.Contains(s, "CD"){
-            s = strings.Replace(s, "CD", "", 1)
-        } else  { found = false}
-    }   
+	for {
+		ok, subStr := hasEither(s)
+		if !ok {
+			break
+		}
+		s = strings.Replace(s, subStr, "", 1)
+	}
 
-    return len(s)
+	fmt.Println(s)
+	return len(s)
 }
+
+func hasEither(s string) (bool, string) {
+	if strings.Contains(s, "AB") {
+		return true, "AB"
+	} else if strings.Contains(s, "CD") {
+		return true, "CD"
+	} else {
+		return false, ""
+	}
+}
+
+// func minLength(s string) int {
+//     if !strings.Contains(s, "AB") && !strings.Contains(s, "CD") {return len(s)}
+//     found := true
+//     for found {
+//         if strings.Contains(s, "AB"){
+//             s = strings.Replace(s, "AB", "", 1)
+//         } else if strings.Contains(s, "CD"){
+//             s = strings.Replace(s, "CD", "", 1)
+//         } else  { found = false}
+//     }
+
+//     return len(s)
+// }
