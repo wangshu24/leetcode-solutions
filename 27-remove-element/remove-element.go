8b54a534b2572162820f0bbd52c0819d1cd50875
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-09 10:40:15 +1300

    Time: 2 ms (36.46%) | Memory: 2.4 MB (57.74%) - LeetSync

--- 27-remove-element/remove-element.go
+++ 27-remove-element/remove-element.go
@@ -1,12 +1,10 @@
 func removeElement(nums []int, val int) int {
-    k := 0
-    for i:=0; i < len(nums); i++ {
-        if nums[i] != val{
-            fmt.Println(nums[i])
-            nums[k] = nums[i]    
-            k++ 
-        }        
+    k :=0 
+    for i:=0; i<len(nums);i++{
+        if nums[i]!=val{
+            nums[k] = nums[i]
+            k++
+        }
     }
-
     return k
 }
