c96d3cf45b5095e0558f0bb794d1943a6f06065d
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-08 22:59:53 +1300

    Time: 9 ms (48.44%) | Memory: 4.5 MB (21.54%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.go
+++ 2-add-two-numbers/add-two-numbers.go
@@ -9,7 +9,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
     result := &ListNode{}
     temp := result
     carryOver := 0
-    for l1!=nil  || l2 !=nil || carryOver != 0 {
+    for l1!=nil  || l2 !=nil || carryOver > 0 {
         sum:=carryOver
         if l1!=nil{
             sum+=l1.Val
