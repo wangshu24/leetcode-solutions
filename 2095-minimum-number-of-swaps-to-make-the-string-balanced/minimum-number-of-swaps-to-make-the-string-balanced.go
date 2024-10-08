467aebf3c94f93d094bdf570df0367b6bf3f385d
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-08 21:27:22 +1300

    Time: 49 ms (5.00%) | Memory: 7.7 MB (100.00%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.go
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.go
@@ -1,11 +1,9 @@
 func minSwaps(s string) int {
     swap :=0
-    //half := len(s)/2
     opening := 0
     for i:=0; i < len(s);i++{
         if string(s[i]) == "["  {opening++} else {
             if opening > 0 {opening--} else {
-                fmt.Println(i)
                 swap++
                 opening++
             }
@@ -15,13 +13,3 @@ func minSwaps(s string) int {
 
     return swap
 }
-
-// func mirror(s1, s2 string) bool {
-//     if s1 == "[" && s2 == "]" {
-//         return true
-//     } else if s1 == "]" && s2 == "[" {
-//         return true
-//     } else {
-//         return false
-//         }
-// }
