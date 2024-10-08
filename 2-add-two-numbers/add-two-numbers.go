bd5289d8ea22181765eb75f4291f96769c63df8c
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-08 22:59:18 +1300

    Time: 3 ms (92.72%) | Memory: 4.5 MB (21.54%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    result := &ListNode{}
+    temp := result
+    carryOver := 0
+    for l1!=nil  || l2 !=nil || carryOver != 0 {
+        sum:=carryOver
+        if l1!=nil{
+            sum+=l1.Val
+            l1 = l1.Next  
+        }
+        if l2!=nil{
+            sum+=l2.Val
+            l2 = l2.Next  
+        }
+        temp.Next = &ListNode{ Val : sum%10, Next : nil}
+        carryOver = sum/10
+        temp = temp.Next
+    }
+
+    return result.Next
+}
