03c49d3a046022c8dbfd9dd19807174c9634e97f
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-12 22:41:15 +1300

    Time: 147 ms (100.00%) | Memory: 28.5 MB (22.22%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.go
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.go
@@ -1,52 +1,31 @@
-// func minGroups(intervals [][]int) int {
-//     start := intervals[0][0]
-//     end := intervals[0][1]
-//     endArr := []int{}
-//     for i:=0; i <len(intervals);i++{
-//         if intervals[i][0] < start { start = intervals[i][0] }
-//         if intervals[i][1] > end { end = intervals[i][1] } 
-//     }
-//     minGroup := len(intervals)
-//     for i:=start; i<end;i++{
-        
-//     }
-    
-//     fmt.Println(start)
-//     fmt.Println(end)
-//     return 0
-// }
-
-
-import (
-    "container/heap"
-    "sort"
-)
-type MinHeap []int
-func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *MinHeap) Push(x interface{}) {
-    *h = append(*h, x.(int))
-}
-func (h *MinHeap) Pop() interface{} {
-    old := *h
-    n := len(old)
-    x := old[n-1]
-    *h = old[0 : n-1]
-    return x
-}
 func minGroups(intervals [][]int) int {
-    sort.Slice(intervals, func(i, j int) bool {
-        return intervals[i][0] < intervals[j][0]
-    })
-    pq := &MinHeap{}
-    heap.Init(pq)
-    for _, interval := range intervals {
-        start, end := interval[0], interval[1]
-        if pq.Len() > 0 && (*pq)[0] < start {
-            heap.Pop(pq)
-        }
-        heap.Push(pq, end)
+    if len(intervals) == 1 {return 1}
+    start := math.MaxInt
+    end := math.MinInt
+    
+    for _,interval:= range intervals{
+        start = min(start, interval[0])
+        end = max(end, interval[1])
     }
-    return pq.Len()
+
+    // for i:=0; i <len(intervals);i++{
+    //     if intervals[i][0] < start { start = intervals[i][0] }
+    //     if intervals[i][1] > end { end = intervals[i][1] } 
+    // }
+
+    endArr := make([]int, end+2)
+    for _,interval:= range intervals{
+        endArr[interval[0]]++
+        endArr[interval[1]+1]--
+    }
+
+    groups := 0
+    maxConcurrentIntervals := 0
+    for i:=start; i<=end;i++{
+        groups += endArr[i]
+        maxConcurrentIntervals = max(maxConcurrentIntervals, groups)
+    }
+
+ 
+    return maxConcurrentIntervals
 }
