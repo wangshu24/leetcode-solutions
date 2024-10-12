cf79c4d15a80558af67fbb82b4af8b40097b7a4f
Elias Tran <trandinhphuc1997@gmail.com>
2024-10-12 23:00:52 +1300

    Time: 173 ms (88.89%) | Memory: 41.9 MB (11.11%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.go
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.go
@@ -1,5 +1,8 @@
 func minGroups(intervals [][]int) int {
     if len(intervals) == 1 {return 1}
+    //Start by initialize 2 anchor int at 2 end
+    //With the beginning anchor has largest int const to begin count down
+    //And vice versa, smallest int const to for ending anchor to begin count up
     start := math.MaxInt
     end := math.MinInt
     
@@ -8,9 +11,17 @@ func minGroups(intervals [][]int) int {
         end = max(end, interval[1])
     }
 
+    //We make a array to record occurence of beginning and end of each int
+    //With positive value = more interval starts with this int
+    //With negative value = more interval ends with this int
+    //We add 2 to slices len since we take the end_int+1 in both val and index, which makes total_len = maxIndex+1 
     endArr := make([]int, end+2)
     for _,interval:= range intervals{
+        //For every start val, we record its occurence
         endArr[interval[0]]++
+        //For every end val, we record its closest subsequent start int with negative val
+        //Denote the decrease in new group because end_val+1 is closest to end_val
+        //And if that int does exist, which means it is included in the same group, does not need to record new occurence
         endArr[interval[1]+1]--
     }
 
@@ -18,6 +29,12 @@ func minGroups(intervals [][]int) int {
     maxConcurrentIntervals := 0
     for i:=start; i<=end;i++{
         groups += endArr[i]
+        //Everytime, we record the max occurence possible because as we loop through, 
+        //it will start encountering negative index, signify the closing of an interval group
+        //therefore, need to record val before it goes to 0
+        //that max is the min number of interval group
+        //the algo already calc the most optimal/least no of group
+        //hence, algo max = question expected min val
         maxConcurrentIntervals = max(maxConcurrentIntervals, groups)
     }
 
