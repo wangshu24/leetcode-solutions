96b020a64d5cb39020fd2101790e2ff651ddb74a
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-03 22:32:36 +1300

    Time: 943 ms (28.57%) | Memory: 132.3 MB (35.71%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.ex
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.ex
@@ -1,12 +1,7 @@
 defmodule Solution do
   @spec make_fancy_string(s :: String.t) :: String.t
-  def make_fancy_string(s), do: do_make_fancy_string(s, nil, nil, <<>>)
-
-  defp do_make_fancy_string(<<ch, rest::bytes>>, prev_ch, prev2_ch, acc) do
-    cond do
-      ch == prev_ch && ch == prev2_ch -> do_make_fancy_string(rest, prev_ch, prev2_ch, acc)
-      true -> do_make_fancy_string(rest, ch, prev_ch, <<acc::bytes, ch>>)
-    end
+  # Regex:
+  def make_fancy_string(s) do
+    String.replace(s, ~r/(.)\1+/, ~S"\1\1")
   end
-  defp do_make_fancy_string(_, _, _, acc), do: acc
 end
