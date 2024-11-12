e6b47c6f2cf496381bb15624bf6478d8ad2d8e4b
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-12 20:28:03 +1300

    Time: 36 ms (98.55%) | Memory: 81 MB (68.64%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
@@ -1,32 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
-        Arrays.sort(items, Comparator.comparingInt(a -> a[0]));
-
-        List<int[]> priceBeauty = new ArrayList<>();
-        int maxBeauty = 0;
+        Arrays.sort(items, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int currMaxBeauty = 0;
+        TreeMap<Integer, Integer> m = new TreeMap<>();
         for (int[] item : items) {
-            maxBeauty = Math.max(maxBeauty, item[1]);
-            priceBeauty.add(new int[]{item[0], maxBeauty});
+            if (item[1] <= currMaxBeauty) {
+                continue;
+            }
+            currMaxBeauty = item[1];
+            m.put(item[0], currMaxBeauty);
         }
-
-        int[] result = new int[queries.length];
+        int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            int query = queries[i];
-            int idx = binarySearch(priceBeauty, query);
-            result[i] = idx != -1 ? priceBeauty.get(idx)[1] : 0;
+            Map.Entry<Integer, Integer> entry = m.floorEntry(queries[i]);
+            if (entry != null) {
+                res[i] = entry.getValue();   
+            }
         }
-        return result;
-    }
-
-    private int binarySearch(List<int[]> priceBeauty, int query) {
-        int left = 0, right = priceBeauty.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (priceBeauty.get(mid)[0] <= query) left = mid + 1;
-            else right = mid - 1;
-        }
-        return right;
+        return res;
     }
 }
