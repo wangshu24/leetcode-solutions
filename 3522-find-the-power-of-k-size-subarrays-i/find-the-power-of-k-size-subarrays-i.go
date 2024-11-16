e2eec2f63be232c668cd9698bb3802ec06b7b1d3
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-16 20:42:51 +1300

    Time: 0 ms (100.00%) | Memory: 7.1 MB (14.29%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.go
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.go
@@ -1,28 +1,29 @@
 func resultsArray(nums []int, k int) []int {
-    res := make([]int, 0)
-    l := 0
-    consecCnt := 1
-    
-    for r := 0; r < len(nums); r++ {
-        if r > 0 && nums[r-1]+1 == nums[r] {
-            consecCnt++
-        }
-        
-        if r-l+1 > k {
-            if nums[l]+1 == nums[l+1] {
-                consecCnt--
-            }
-            l++
-        }
-        
-        if r-l+1 == k {
-            if consecCnt == k {
-                res = append(res, nums[r])
-            } else {
-                res = append(res, -1)
-            }
-        }
+    if k == 1 {
+        return nums
     }
     
-    return res
+    n := len(nums)
+    result := make([]int, 0)
+    left := 0
+    right := 1
+    
+    for right < n {
+        isNotConsecutive := nums[right] - nums[right-1] != 1
+        
+        if isNotConsecutive {
+            for left < right && left + k - 1 < n {
+                result = append(result, -1)
+                left++
+            }
+            left = right
+        } else if right - left == k - 1 {
+            result = append(result, nums[right])
+            left++
+        }
+        
+        right++
+    }
+    
+    return result
 }
