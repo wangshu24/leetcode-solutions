1cbc03e21bc03bd0add847e6acedd4359a0c2311
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-20 21:39:13 +1300

    Time: 861 ms (10.00%) | Memory: 8.5 MB (25.00%) - LeetSync

---
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.go
@@ -0,0 +1,35 @@
+func takeCharacters(s string, k int) int {
+    remove, l := 0, 0
+    kmap := make([]int, 3)
+ 
+    if len(s) < k*3 {return -1}
+    for i:=0; i < len(s); i++{
+        kmap[s[i]-97]++
+    }
+    
+    if kmap[0] < k || kmap[1] < k || kmap[2] < k {return -1}
+    fmt.Println(kmap)
+    for r:=0; r< len(s); r++{
+        kmap[s[r]-97]--
+
+        if slices.Min(kmap) < k {
+            fmt.Println("left is: ", l)
+            fmt.Println("right is: ", r)
+            for slices.Min(kmap) < k {
+                kmap[s[l]-97]++
+                l++
+            }
+           
+        } else {
+            fmt.Println(r-l)
+            remove = max(remove, r-l+1)
+        }
+    }
+
+    return len(s)-remove
+}
+
+func max(a,b int) int{
+    if a > b {return a}
+    return b
+}
