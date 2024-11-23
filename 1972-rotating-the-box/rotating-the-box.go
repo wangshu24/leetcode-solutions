c0b376ecf2c870e6ac8329d1215f0008a6c94dd6
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-23 20:33:26 +1300

    Time: 568 ms (25.00%) | Memory: 19 MB (9.09%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.go
@@ -0,0 +1,38 @@
+// Rock # = 35
+// Block * = 42
+// Empty . = 46
+
+func rotateTheBox(box [][]byte) [][]byte {
+    for _,row := range box {
+        e:= len(row)-1        
+        for i:= len(row)-1; i > -1; i-- {
+            if row[i] == 46 {
+                continue
+            } else if row[i] == 35 {
+                stone:= row[i]
+                row[e] = stone
+                if e!=i {row[i] = 46}
+                e--
+            } else {
+                e = i-1
+            }
+        }   
+    }
+
+    col := len(box)
+    row := len(box[0])
+    rBox := make([][]byte, row)   
+
+    for ind := range rBox {
+        rBox[ind] = make([]byte, col)
+    }
+
+    for i:=0; i < row; i++ {
+        for j:= col-1; j > -1; j-- {
+            // fmt.Println("rBox coord: ", i, " ",col-j-1, "box coord: ", j, " ", i)
+            rBox[i][col-j-1] = box[j][i]
+        }
+    }
+    
+    return rBox 
+}
