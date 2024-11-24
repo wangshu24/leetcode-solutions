915a70e7240dd5dcdd5b9be7022bdb833e310152
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-24 16:37:01 +1300

    Time: 0 ms (100.00%) | Memory: 9.3 MB (50.00%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.go
@@ -0,0 +1,34 @@
+func maxMatrixSum(mat [][]int) int64 {
+    res:=0
+    
+    minVal := int(math.Pow(10,5))
+    countNeg:= 0 
+    for _,row := range mat {
+        
+        for ind,val := range row {
+            if val <= 0 {
+                countNeg++
+                row[ind] = val * -1
+                minVal = min(minVal, row[ind])
+                res = res + row[ind]
+            } else {
+                minVal = min(minVal, val)
+                res = res + val
+            }
+        }      
+    }
+
+    if countNeg%2 != 0 {
+        res = res - (minVal*2)
+    }
+
+    fmt.Println(res)
+    return int64(res)
+}
+
+func min (a,b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
