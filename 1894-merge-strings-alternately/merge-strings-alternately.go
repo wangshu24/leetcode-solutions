207908135af75ec289f7cb527386828ef6ff16f4
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-25 13:04:04 +1300

    Time: 0 ms (100.00%) | Memory: 3.8 MB (61.51%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.go
+++ 1894-merge-strings-alternately/merge-strings-alternately.go
@@ -6,20 +6,17 @@ func mergeAlternately(word1 string, word2 string) string {
     var res strings.Builder
     res.Grow(len(word1) + len(word2))
     lengthMin := min(len(word1), len(word2))
-    lengthMax := max(len(word1), len(word2))
+    
 
     for i:= 0; i < lengthMin; i++{
         res.WriteString(string(word1[i]))
         res.WriteString(string(word2[i]))
     }
 
-    longer := ""
-    if len(word1) == lengthMax {
-        longer = word1
-    }  else { longer = word2 }
-
-    for i:=lengthMin; i < lengthMax; i++{
-        res.WriteString(string(longer[i]))
+    if len(word1) < len(word2){
+        res.WriteString(string(word2[len(word1):]))
+    } else {
+        res.WriteString(string(word1[len(word2):]))
     }
 
     return res.String()
