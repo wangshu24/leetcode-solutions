a41da831c43756c003e9da614b40e87837f1c11d
Elias Tran <trandinhphuc1997@gmail.com>
2024-11-26 14:28:06 +1300

    Time: 23 ms (11.11%) | Memory: 9.8 MB (8.33%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.go
@@ -0,0 +1,28 @@
+func findChampion(n int, edges [][]int) int {
+    if len(edges) == 0  {
+        if n == 1 {
+            return 0
+        } else {
+            return -1
+        }
+    }
+    team := map[int]int{}
+    
+    for _,ed := range edges {
+        if team[ed[0]] == 0 { team[ed[0]] = 0 }
+        team[ed[1]]++
+    }
+    fmt.Println(team)
+    res:= []int{}
+    teamFound := 0
+    for key,val := range team{
+        teamFound++
+        if val == 0 {
+            res = append(res, key)
+        }
+    }
+    if teamFound < n {return -1}
+    if len(res) > 1 {return -1}
+
+    return res[0]
+}   
