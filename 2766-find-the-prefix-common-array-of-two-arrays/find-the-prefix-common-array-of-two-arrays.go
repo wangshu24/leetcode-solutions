3856cbe4b178202297d1a3b7134a623a021b3b40
Elias Tran <trandinhphuc1997@gmail.com>
2025-01-14 02:34:44 -0500

    Time: 0 ms (100.00%) | Memory: 8 MB (84.81%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.go
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.go
@@ -2,7 +2,7 @@ func findThePrefixCommonArray(A []int, B []int) []int {
     freq  := make([]int, len(A)+1)
     res := make([]int, len(A))
     comm := 0
-    fmt.Println(res)
+
     for i:=0; i < len(A); i++ {
         freq[A[i]]++
         if freq[A[i]] == 2 {comm++}
