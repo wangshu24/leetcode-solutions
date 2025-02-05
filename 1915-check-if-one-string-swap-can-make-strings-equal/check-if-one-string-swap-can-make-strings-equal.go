d204e3406fe99ef205763c1c0e6fbb0b4902c2e4
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-05 09:50:14 -0500

    Time: 0 ms (100.00%) | Memory: 4.1 MB (14.71%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.go
@@ -0,0 +1,36 @@
+// func areAlmostEqual(s1 string, s2 string) bool {
+//     if s1 == s2 {return true}
+//     if len(s1) != len(s2) {return false}
+    
+//     mismatch := 0
+//     for i:=0; i < len(s1); i++ {
+//         if s1[i] != s2[i] {mismatch++}
+//     }
+//     if mismatch == len(s1) {return false}
+//     if mismatch == 2 {
+//         return true
+//     }
+
+//     return false
+// }
+
+func areAlmostEqual(s1 string, s2 string) bool {
+     if s1 == s2 {return true}
+    if len(s1) != len(s2) {return false}
+    l:=0
+    chars1 := map[byte]int{}
+    chars2 := map[byte]int{}
+    for i:=0; i < len(s1) ; i++{
+        if s1[i] != s2[i] {
+            chars1[s1[i]]++
+            chars2[s2[i]]++
+            l++
+        }
+    }
+        if l > 2 {return false}
+    fmt.Println(chars1, chars2) 
+    if maps.Equal(chars1, chars2) {
+        return true
+    }
+    return false
+}
