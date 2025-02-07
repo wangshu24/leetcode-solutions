8b9c1102c7a33dbba43e92533262109e13707040
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-07 10:47:27 -0500

    Time: 147 ms (11.11%) | Memory: 31.8 MB (11.11%) - LeetSync

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.go
@@ -0,0 +1,23 @@
+func queryResults(limit int, queries [][]int) []int {
+    colMap := map[int]int{}
+    ballMap := map[int]int{}
+    res := make([]int, len(queries))
+
+    for i:=0; i < len(queries); i++{
+        col := queries[i][1]
+        ball := queries[i][0]
+
+        if prevCol, ok := ballMap[ball]; ok {
+            colMap[prevCol]--
+            if colMap[prevCol] == 0 {delete(colMap, prevCol)}
+        }
+        
+        ballMap[ball] = col 
+        colMap[col]++
+        res[i] = len(colMap)
+    }
+
+    fmt.Println(colMap)
+    return res
+}
+
