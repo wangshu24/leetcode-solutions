d796c105d90127137e503f78a96926cd89f12fcf
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-09 02:08:19 -0500

    Time: 27 ms (84.62%) | Memory: 14.4 MB (61.54%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.go
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.go
@@ -1,16 +1,12 @@
 func countBadPairs(nums []int) int64 {
-    good := make(map[int]int)
-    res,gp := 0, 0
-
-    for i:=0; i < len(nums); i++ {
-        good[nums[i]-i]++
+    var totalPairs int = (len(nums) * (len(nums) - 1))/2
+    var goodCount int
+    var goods = map[int]int{}
+    for i:=0; i<len(nums); i++ {
+        val := i - nums[i]
+        goodCount += goods[val]
+        goods[val]++
     }
-    for _, val := range good {
-        if val > 1 {
-            gp += (val * (val-1))/2
-        }
-    }
-
-    res = ((len(nums) * (len(nums)-1))/2)-gp
-    return int64(res)
+    
+    return int64(totalPairs - goodCount)
 }
