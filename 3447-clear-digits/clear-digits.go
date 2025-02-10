33a453f11715a1ab1f2e038cf0323ecf84a3b2e5
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-10 01:30:49 -0500

    Time: 5 ms (2.22%) | Memory: 4.6 MB (8.89%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.go
@@ -0,0 +1,25 @@
+func clearDigits(s string) string {
+    var builder strings.Builder
+    mark := make([]int, len(s))
+    stack := make([]int, 0)
+    for i:=0; i < len(s); i++ {
+        if s[i] >= 48 && s[i] <= 57 {
+            mark[i] = 1
+            stack = stack[:len(stack)-1]
+        } else {
+            stack = append(stack,i)
+        }
+    }
+
+    fmt.Println(stack)
+    fmt.Println(mark)
+    
+    for i:=0; i < len(stack); i++ {
+        fmt.Println(s[stack[i]]) 
+        builder.WriteByte(s[stack[i]])
+        
+    }
+    
+
+    return builder.String()
+} 
