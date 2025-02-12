661d41b00d455a19eae50b8f1c7443da28e4f970
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-12 13:03:54 -0500

    Time: 102 ms (13.79%) | Memory: 12.7 MB (8.62%) - LeetSync

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.go
@@ -0,0 +1,32 @@
+func maximumSum(nums []int) int {
+    total := map[int][]int{}
+    slices.Sort(nums)
+    for _, num := range nums {
+        sum := sumDigits(num)
+        total[sum] = append(total[sum], num)
+    }
+    fmt.Println(total)
+    res:=0
+    for _, list := range total {
+        if len(list) >1 {
+            res = max(res, list[len(list)-1] + list[len(list)-2] )
+        }
+    }
+    fmt.Println(res)
+    if res == 0 {res = -1}
+    return res
+}
+
+func sumDigits(num int) int {
+    res := 0
+    for num > 0 {
+        res+=num%10
+        num = num/10
+    }
+    return res
+}
+
+func max(a, b int) int {
+    if a > b {return a}
+    return b
+}
