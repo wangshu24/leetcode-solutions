a618c646e15a10734b076a7f8cf55f645e788cb9
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-13 15:26:14 -0500

    Time: 108 ms (72.73%) | Memory: 14.1 MB (72.73%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.go
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.go
@@ -1,40 +1,41 @@
-type MinHeap []int
 
-func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *MinHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+func minOperations(nums []int, k int) int {
+    h := &MinIntHeap{}
+    heap.Init(h)
+    count := 0
+    for _, num := range nums {
+        if num < k {
+            heap.Push(h, num)
+        }
+    }
+    
+    for h.Len() > 0 {
+        a := heap.Pop(h).(int)
+        if h.Len() == 0 {
+            count++
+            break
+        }
+        b := heap.Pop(h).(int)
+        num := a*2+b
+        if num < k {
+            heap.Push(h, num)
+        }
+        count++
+    }
+    return count
 }
 
-func (h *MinHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	item := old[n-1]
-	*h = old[0 : n-1]
-	return item
-}
 
-func minOperations(a []int, k int) int {
-	h := &MinHeap{}
-	heap.Init(h)
-	for _, val := range a {
-		heap.Push(h, val)
-	}
-    fmt.Println(h)
-	for i := 0; ; i++ {
-		x := heap.Pop(h).(int)
-		if x >= k {
-			return i
-		}
-		y := heap.Pop(h).(int)
-		t := int64(2 * x + y) // test overflow
-		if t < int64(k) {
-			heap.Push(h, int(t))
-		} else {
-			heap.Push(h, k)
-		}
-	}
-	return -1
+
+
+
+type MinIntHeap []int
+func(h MinIntHeap) Len() int { return len(h) }
+func(h MinIntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func(h MinIntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func(h *MinIntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func(h *MinIntHeap) Pop() interface{} {
+    x := (*h)[len(*h)-1]
+    *h = (*h)[:len(*h)-1]
+    return x
 }
