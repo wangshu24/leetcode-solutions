3efafbf5af05f6a6d6aa92f188d7b5cbfdfa8ff8
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-19 00:04:12 -0500

    Time: 0 ms (100.00%) | Memory: 3.8 MB (90.00%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.go
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.go
@@ -1,15 +1,15 @@
 func smallestNumber(pattern string) string {
-  	stack := make([]byte, 0, len(pattern)+1)
-	res := make([]byte, 0, len(pattern)+1)
-    for i :=0; i <= len(pattern); i++ {
+    stack, res := []byte{}, []byte{}
+    
+    for i:=0; i < len(pattern)+1; i++ {
         stack = append(stack, byte(i+'1'))
-        if (i == len(pattern) || pattern[i] == 'I') {
-            for j := len(stack)-1; j >= 0; j-- {
-				res = append(res, stack[j])
-			}
-			stack = stack[:0]
+
+        if i == len(pattern) || pattern[i] == 'I' {
+            for j := len(stack); j > 0; j-- {
+                res = append(res, stack[j-1])
+            }
+            stack  = stack[:0]
         }
-        
     }
     return string(res)
 }
