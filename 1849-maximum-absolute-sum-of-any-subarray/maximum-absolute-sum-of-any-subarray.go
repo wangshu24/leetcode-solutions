8450be5f7ff258481b7e26c61ecdcc5f8bb5e766
Elias Tran <trandinhphuc1997@gmail.com>
2025-02-25 20:36:30 -0500

    Time: 0 ms (100.00%) | Memory: 10.2 MB (35.71%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.go
@@ -0,0 +1,27 @@
+func maxAbsoluteSum(nums []int) int {
+    maxV, minV := nums[0], nums[0]
+    maxEnd, minEnd := nums[0], nums[0]
+    var res int
+
+    for i:=1; i < len(nums); i++ {
+        maxEnd = max(maxEnd+nums[i], nums[i])
+        maxV = max(maxEnd, maxV)
+    }
+
+    for i:=1; i < len(nums); i++{
+        minEnd = min(minEnd + nums[i], nums[i])
+        minV = min(minV, minEnd)
+    }
+    fmt.Println(maxV, minV)
+    res = max(maxV, -minV)
+    return res
+}
+
+func max(a, b int) int { 
+    if a  > b {return a}
+    return b
+}
+func min(a,b  int) int {
+    if a >b {return b}
+    return a
+}
