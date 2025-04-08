9382c4f4536a071131a28cb945b3cda9ad47890a
Elias Tran <trandinhphuc1997@gmail.com>
2025-04-08 10:48:30 -0400

    Time: 2 ms (41.86%) | Memory: 4.8 MB (86.05%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.go
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.go
@@ -1,21 +1,13 @@
 func minimumOperations(nums []int) int {
-    s, l := slices.Min(nums), slices.Max(nums)
-    tmp := make([]int, l-s+1)
-    for _,i := range nums {
-        tmp[i-s]++
-    }
-    var min int
-    if slices.Max(tmp) == 1 {return min}
+    n := len(nums)
+    freq := make([]bool, 101)
 
-    for i:=0; i < len(nums); i+=3{
-        if i >= len(nums) || i+2 >= len(nums) {break}
-        tmp[nums[i]-s]--
-        tmp[nums[i+1]-s]--
-        tmp[nums[i+2]-s]--
-        min++
-        if slices.Max(tmp) == 1 {return min}
+    for i:=n-1; i >= 0; i-- {
+        if freq[nums[i]] {
+            return (i/3)+1
+        }
+        freq[nums[i]] = true
     }
-    
-    if slices.Max(tmp) > 1 {return min+1}
-    return min
+
+    return 0
 }
