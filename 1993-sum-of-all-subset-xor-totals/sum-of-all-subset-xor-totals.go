c8d901ab444fb8f5ccc5b55654e056bdd6709699
Elias Tran <trandinhphuc1997@gmail.com>
2025-04-12 14:18:47 -0400

    Time: 0 ms (100.00%) | Memory: 4 MB (32.75%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.go
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.go
@@ -1,19 +1,17 @@
 func subsetXORSum(nums []int) int {
     var res int
 
-    var backtrack func(i, cursum int)
-    backtrack = func(i, cursum int) {
-        if i == len(nums) {
-            res += cursum
+    var backtrack func(i, cur int)
+    backtrack = func(i, cur int) {
+        if i >= len(nums) {
+            res += cur
             return
-        } else if i > len(nums) {return}
-        prev := cursum
-        cursum = cursum ^ nums[i]
-        backtrack(i+1, cursum)
-        backtrack(i+1, prev)
-    }
+        }
+
+        backtrack(i+1, cur)
+        backtrack(i+1, cur ^ nums[i])
+    } 
 
     backtrack(0, 0)
-
     return res
 }
