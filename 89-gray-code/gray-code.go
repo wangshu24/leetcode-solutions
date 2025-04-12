07e324119edf4090d5cac7fcdbd622c5eb7c21a6
Elias Tran <trandinhphuc1997@gmail.com>
2025-04-12 15:32:14 -0400

    Time: 0 ms (100.00%) | Memory: 8.5 MB (72.84%) - LeetSync

--- 89-gray-code/gray-code.go
+++ 89-gray-code/gray-code.go
@@ -1,7 +1,26 @@
 func grayCode(n int) []int {
-    res := make([]int, 1 << n)
-    for i:=0; i < 1<<n ; i++ {
-        res[i] = i ^ (i >> 1)
+    if n == 0 {return []int{0}}
+
+    if n == 1 {return []int{0,1}}
+    
+    s1 := grayCode(n-1)
+
+    s2 := make([]int, len(s1) * 2)
+
+    copy(s2, s1)
+    for i := len(s1); i < len(s1) * 2; i++ {
+        s2[i] = i ^ (i >> 1)
     }
-    return res
+    
+    return s2
 }
+
+// func grayCode(n int) []int {
+//     res := make([]int, 1 << n)
+
+//     for i:=0; i < 1 << n; i++ {
+//         res[i] = i ^ (i >> 1)
+//     }
+
+//     return res
+// }
