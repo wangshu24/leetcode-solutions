3927965a1fbc8557afdc99e08e741e0a23f3fffa
Elias Tran <trandinhphuc1997@gmail.com>
2026-04-23 00:54:07 -0400

    Time: 75 ms (81.23%) | Memory: 72.1 MB (77.20%) - LeetSync

--- 146-lru-cache/lru-cache.go
+++ 146-lru-cache/lru-cache.go
@@ -1,66 +1,75 @@
 type Node struct {
-    key, val    int
+    key, val int
     left, right *Node
 }
 
 type LRUCache struct {
-    nodes      map[int]*Node
-    capacity   int
-    head, tail *Node // Sentinel nodes
+    nodes map[int]*Node
+    head, tail *Node
+    capt int
+    leng int
 }
 
+
 func Constructor(capacity int) LRUCache {
-    // Initialize dummy nodes
-    head := &Node{0, 0, nil, nil}
-    tail := &Node{0, 0, nil, nil}
-    head.right = tail
-    tail.left = head
-    
-    return LRUCache{
-        nodes:    make(map[int]*Node),
-        capacity: capacity,
-        head:     head,
-        tail:     tail,
+    head:= &Node{0,0,nil,nil}
+    tail:= &Node{0,0,nil,nil}
+    head.left = tail
+    tail.right = head
+    return LRUCache {
+        nodes: make(map[int]*Node),
+        head: head,
+        tail: tail,
+        capt: capacity,
+        leng: 0,
     }
 }
 
-// Helper to remove a node from the list
-func (this *LRUCache) remove(n *Node) {
+func remove(n *Node) {
     n.left.right = n.right
     n.right.left = n.left
 }
 
-// Helper to add a node right after the dummy head
-func (this *LRUCache) addToHead(n *Node) {
-    n.right = this.head.right
-    n.left = this.head
-    this.head.right.left = n
-    this.head.right = n
+func (this *LRUCache) moveToHead(n *Node) {
+    n.right = this.head
+    n.left = this.head.left
+    this.head.left.right = n
+    this.head.left = n
+
 }
 
+
 func (this *LRUCache) Get(key int) int {
-    if node, ok := this.nodes[key]; ok {
-        this.remove(node)
-        this.addToHead(node)
+    if node, there := this.nodes[key]; there {
+        remove(node)
+        this.moveToHead(node)
         return node.val
     }
     return -1
 }
 
-func (this *LRUCache) Put(key int, value int) {
-    if node, ok := this.nodes[key]; ok {
-        this.remove(node)
+
+func (this *LRUCache) Put(key int, value int)  {
+    if node, there := this.nodes[key]; there {
+        remove(node)
+        this.moveToHead(node)
         node.val = value
-        this.addToHead(node)
     } else {
-        if len(this.nodes) >= this.capacity {
-            // Remove the real tail (the node before dummy tail)
-            lruNode := this.tail.left
-            this.remove(lruNode)
-            delete(this.nodes, lruNode.key)
+        if len(this.nodes) >= this.capt {
+            lrunode := this.tail.right
+            remove(lrunode)
+            delete(this.nodes, lrunode.key)
         }
         newNode := &Node{key: key, val: value}
         this.nodes[key] = newNode
-        this.addToHead(newNode)
+        this.moveToHead(newNode)
     }
 }
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
