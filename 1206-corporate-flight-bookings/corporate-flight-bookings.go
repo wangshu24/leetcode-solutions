400e038d832a61f5f6c0a4cfa69276c557134646
Elias Tran <trandinhphuc1997@gmail.com>
2026-04-22 23:15:34 -0400

    Time: 4 ms (28.13%) | Memory: 10 MB (31.25%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.go
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.go
@@ -5,11 +5,9 @@ func corpFlightBookings(bookings [][]int, n int) []int {
         res[book[0]-1] += book[2]
         res[book[1]] -= book[2]
     }
-    fmt.Println(res)
     for i:=1; i < len(res); i++ {
         res[i] += res[i-1]
     }
-    fmt.Println(res)
 
     return res[:n]
 }
