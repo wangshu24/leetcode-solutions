1e294317e6022de78a283a234e43697beccd506d
Elias Tran <trandinhphuc1997@gmail.com>
2026-04-22 23:15:18 -0400

    Time: 26 ms (14.06%) | Memory: 11.1 MB (7.81%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.go
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.go
@@ -1,18 +1,15 @@
 func corpFlightBookings(bookings [][]int, n int) []int {
-    var last int
-    for _,val := range bookings {
-        last = max(last, val[1])
-    } 
-    resLen := max(last, len(bookings))
-    res := make([]int, resLen)
-    for _, val := range bookings {
-        s, e := val[0], val[1]
-        res[s-1]+=val[2]
-        if e < len(res) {res[e]-=val[2]}
-    }
+    res := make([]int, n+1)
 
-    for i:=1; i < len(res); i++ {
-        res[i] = res[i-1] + res[i]
+    for _, book := range bookings {
+        res[book[0]-1] += book[2]
+        res[book[1]] -= book[2]
     }
-    return res
+    fmt.Println(res)
+    for i:=1; i < len(res); i++ {
+        res[i] += res[i-1]
+    }
+    fmt.Println(res)
+
+    return res[:n]
 }
