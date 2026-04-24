4002ed9be3457ca84ff43878cf59156ea4c2670a
Elias Tran <trandinhphuc1997@gmail.com>
2026-04-24 15:44:31 -0400

    Time: 122 ms (5.14%) | Memory: 20.6 MB (5.14%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.go
@@ -0,0 +1,24 @@
+import (
+    "slices" 
+    "cmp"
+)
+
+func findMinArrowShots(points [][]int) int {
+    slices.SortFunc(points, func(a, b []int) int {
+            return cmp.Compare(a[1], b[1])
+    })
+    fmt.Println(points)
+
+    res, minx := 1, points[0][1]
+    for _, point := range points {
+        if minx >= point[0] {
+            continue
+        } else {
+            minx = point[1]
+            res++
+        }
+    }
+
+
+    return res
+}
