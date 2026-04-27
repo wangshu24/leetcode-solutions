f70dddee29e79189700ffac2b7bc374a9d5f4b0b
Elias Tran <trandinhphuc1997@gmail.com>
2026-04-27 00:33:43 -0400

    Time: 100 ms (18.31%) | Memory: 8 MB (33.80%) - LeetSync

--- 312-burst-balloons/burst-balloons.go
+++ 312-burst-balloons/burst-balloons.go
@@ -1,26 +1,30 @@
 func maxCoins(nums []int) int {
-	temp := make([]int, 0, len(nums)+2)
-	temp = append(temp, 1)
-	for _, v := range nums {
-		
-			temp = append(temp, v)
-		
-	}
-	temp = append(temp, 1)
-	n := len(temp)
-	dp := make([][]int, n)
-	for i := range dp {
-		dp[i] = make([]int, n)
-	}
+    nums = slices.Concat([]int{1}, nums)
+    nums = append(nums, 1)
 
-	for length := 2; length < n; length++ {
-		for i := 0; i < n-length; i++ {
-			j := i + length
-			for k := i + 1; k < j; k++ {
-				currentBurst := temp[i] * temp[k] * temp[j]
-				dp[i][j] = max(dp[i][j], dp[i][k]+dp[k][j]+currentBurst)
-			}
-		}
-	}
-	return dp[0][n-1]
+    dp := make([][]int, len(nums))
+    for i:=0; i < len(nums); i++ {
+        dp[i] = make([]int, len(nums))
+    }
+
+    var dfs func(int, int) int
+    dfs = func(l, r int) int{
+        if l > r {
+            return 0
+        }
+
+        if dp[l][r] > 0 {
+            return dp[l][r]
+        }
+
+        dp[l][r] = 0
+        for i:=l; i <= r; i++ {
+            coins := nums[l-1] * nums[i] * nums[r+1]
+            coins += dfs(l, i-1) + dfs(i+1, r)
+            dp[l][r] = max(dp[l][r], coins) 
+        }
+        return dp[l][r]
+    }
+    
+    return dfs(1, len(nums)-2)
 }
