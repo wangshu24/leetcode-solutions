cffa46cdd76b5901ef86128f7d35c98e3ed3b64a
Elias Tran <trandinhphuc1997@gmail.com>
2026-04-27 16:13:26 -0400

    Time: 0 ms (100.00%) | Memory: 4 MB (88.69%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.go
+++ 410-split-array-largest-sum/split-array-largest-sum.go
@@ -1,59 +1,29 @@
-import (
-	"math"
-)
-
 func splitArray(nums []int, k int) int {
-	n := len(nums)
-	// Memoization table: key is [2]int{index, remainingK}
-	memo := make(map[[2]int]int)
+    minsub := slices.Max(nums)
+    var maxsub int 
+    for _, val := range nums {
+        maxsub+=val
+    }
 
-	var dfs func(idx int, cuts int) int
-	dfs = func(idx int, cuts int) int {
-		// Base Case 1: If only one partition left, the result is the sum of the rest
-		if cuts == 1 {
-			sum := 0
-			for i := idx; i < n; i++ {
-				sum += nums[i]
-			}
-			return sum
-		}
+    for minsub < maxsub {
+        mid := minsub + (maxsub - minsub)/2
+        split, sum := 1, 0
+        for i:=0; i < len(nums);i++ {
+            if sum+nums[i] > mid {
+                split++
+                sum = nums[i]
+            } else {
+                sum+= nums[i]
+            }
+        }
+        if split > k {
+            minsub = mid+1
+        } else {
+            maxsub = mid
+        }
+    }
 
-		// Check Memo
-		state := [2]int{idx, cuts}
-		if val, ok := memo[state]; ok {
-			return val
-		}
 
-		minLargestSum := math.MaxInt32
-		currentSum := 0
-
-		// Base Case 2: Optimization - ensure enough elements left for remaining cuts
-		// We can only iterate until we have (cuts-1) elements remaining for the future splits
-		for i := idx; i <= n-cuts; i++ {
-			currentSum += nums[i]
-			
-			// Get max sum of the remaining splits
-			remainingMax := dfs(i+1, cuts-1)
-			
-			// We want the maximum of (current_subarray_sum, result_of_sub_problem)
-			// Then we want to minimize this value across all possible split points
-			maxVal := max(currentSum, remainingMax)
-			minLargestSum = min(minLargestSum, maxVal)
-		}
-
-		memo[state] = minLargestSum
-		return minLargestSum
-	}
-
-	return dfs(0, k)
+    return minsub
 }
 
-func min(a, b int) int {
-	if a < b { return a }
-	return b
-}
-
-func max(a, b int) int {
-	if a > b { return a }
-	return b
-}
