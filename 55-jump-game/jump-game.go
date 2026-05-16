4962bff64316b135c93de738ecc620580fc595ad
Elias Tran <trandinhphuc1997@gmail.com>
2026-05-16 19:16:32 -0400

    Time: 0 ms (100.00%) | Memory: 9.2 MB (42.93%) - LeetSync

--- 55-jump-game/jump-game.go
+++ 55-jump-game/jump-game.go
@@ -1,28 +1,10 @@
 func canJump(nums []int) bool {
-    jumps := make([]int, len(nums))
-    for ind, val := range nums {
-        jumps[ind] = val
+    dest := len(nums)-1
+    for i:= len(nums)-2; i >= 0; i-- {
+        if nums[i] + i >= dest {
+            dest = i
+        } 
     }
-    end := make([]int, len(nums))
     
-    var back func( int) bool 
-    back = func(cur int) bool {
-        if end[cur] == -1 {
-            return false
-        }
-        if cur >= len(nums)-1 || end[cur] == 1 {
-            return true
-        }
-        
-        can := false
-        for i:=1; i <= jumps[cur]; i++ {
-            can = can || back(cur+i)
-        }
-        if !can {
-            end[cur] = -1
-        }        
-        return can
-    }
-
-    return back(0)
+    return dest == 0
 }
