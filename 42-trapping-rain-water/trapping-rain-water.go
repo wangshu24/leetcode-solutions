76bfe89e1c9aa5cb606961eb85a2a89e58a9d865
Elias Tran <trandinhphuc1997@gmail.com>
2026-05-18 11:32:52 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (78.07%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,18 +1,21 @@
 func trap(height []int) int {
-    l,r := 0,len(height)-1
-    mL, mR := height[0], height[len(height)-1]
-    res := 0
-    for l < r {
-        if mL < mR {
-            l++ 
-            mL = max(mL, height[l])
-            res += mL - height[l]
-        } else {
-            r--
-            mR = max(mR, height[r])
-            res += mR - height[r]
+    q := make([]int,0)
+    total := 0
+
+    for i:=0; i < len(height); i ++ {
+        for len(q) > 0 && height[i] > height[q[len(q)-1]] {
+            bId := q[len(q)-1]
+            q = q[:len(q)-1]
+            if len(q) == 0 {
+                break
+            }
+            lW := q[len(q)-1]
+            minH := min(height[i], height[lW]) - height[bId]
+            w := i - lW -1
+            total += minH * w
         }
+        q = append(q, i)
     }
 
-    return res
+    return total
 }
